f6c9d35726bce060217c36b1c69e128276d42904
Jaem Dessources <jaem.developer@gmail.com>
2025-06-26 15:21:14 -0400

    completed chapter 3

--- src/app/exercises/api/posts/route.exercise.tsx
+++ src/app/exercises/api/posts/route.exercise.tsx
@@ -1,12 +1,11 @@
 // 🐶 Importe la fonction `getPosts` qui va récupérer les posts en BDD
-//import {getPosts} from '@/db/sgbd'
+import {getPosts} from '@/db/sgbd'
 
 export async function GET() {
   // 🐶 Appelle la fonction `getPosts` pour récupérer les posts en BDD
+  const response = await getPosts()
 
-  // 🐶 Retourne les données en json dans la réponse http
-  // ⛏️ Supprime `[]` et remplace le par la `data`
-  return Response.json([])
+  return Response.json(response)
   // 🐶 Constate la présence des données ici:  http://localhost:3000/exercises/api/posts
 
   // 🐶 N'oublie pas les exercices bonus
