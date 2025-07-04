96de8bd07295884a3b59be986833d3f9fcee4f60
Jaem Dessources <jaem.developer@gmail.com>
2025-07-04 17:22:52 -0400

    completed chapter 8

--- src/app/exercises/shop-admin/actions.exercise.tsx
+++ src/app/exercises/shop-admin/actions.exercise.tsx
@@ -10,7 +10,7 @@ import {Product} from '@/lib/type'
 import {formSchema} from './schema'
 
 type FormStateSimple = {error: boolean; message: string}
-
+const formSchemaLight = formSchema.partial({id: true, createdAt: true})
 // 🐶 Rappel : Avec `useActionState` l'action server doit avoir 2 paramètres (`state` et `FormData`)
 export async function onSubmitAction(
   prevState: FormStateSimple,
@@ -21,14 +21,23 @@ export async function onSubmitAction(
   console.log('data', data)
   // 🐶 Valide les données avec `Zod`
   // 🤖
-  // const formData = Object.fromEntries(data)
-  // const parsed = formSchema.safeParse(formData)
+  const formData = Object.fromEntries(data)
+  const parsed = formSchemaLight.safeParse(formData)
 
   // 🐶 Si les données ne sont pas valides (`if (!parsed.success)`), retourne un objet de type `FormStateSimple`
   // 🤖 Aide toi de `logZodError(data)` pour afficher les erreurs
+  if (!parsed.success) {
+    logZodError(data)
+    return {error: true, message: 'Données invalides'}
+  }
 
   // 🐶 Appelle la BDD dans un `try` `catch` avec :
   // 🤖 await persistProductDao(parsed.data)
+  try {
+    await persistProductDao(parsed.data as Product)
+  } catch (error) {
+    console.error(error)
+  }
 
   return {error: false, message: 'Success'}
 }
--- src/app/exercises/shop-admin/form/use-action-state.exercise.tsx
+++ src/app/exercises/shop-admin/form/use-action-state.exercise.tsx
@@ -10,40 +10,49 @@ import {
   Select,
 } from '@/components/ui/select'
 
-import {useRef} from 'react'
+import React, {useRef} from 'react'
 import {CategoriesEnum, Product} from '@/lib/type'
 // 🐶 Importe `onSubmitAction` notre action server
-//import {onSubmitAction} from '../actions'
+import {onSubmitAction} from '../actions'
 
 // 🐶 Importe `useActionState`
-//import {useActionState} from 'react'
+import {useActionState} from 'react'
 import {Label} from '@/components/ui/label'
+import {toast} from 'sonner'
+import {useFormStatus} from 'react-dom'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function ProductForm({product}: {product?: Product}) {
   // 🐶 Utilise le hook `useActionState` pour gérer l'état de notre formulaire
-  // const [state, formAction] = useActionState
-  // 🐶 Passe `onSubmitAction` en premier argument et
-  // {error: false, message: ''} en deuxième argument le state initial
+  const [state, formAction] = useActionState(onSubmitAction, {
+    error: false,
+    message: '',
+  })
 
   const formRef = useRef<HTMLFormElement>(null)
 
   // 🐶 Utilise `React.useEffect` pour afficher un message en fonction de l'état de notre formulaire et reset le `form`
   // 🤖 toast.error(state.message) ou toast.success(state.message)
   // 🤖 handleReset() pour réinitialiser le formulaire
+  React.useEffect(() => {
+    if (state.error) toast.error(state.message)
+    else {
+      toast.success(state.message)
+      handleReset()
+    }
+  }, [state])
 
-  // const handleReset = () => {
-  //   if (formRef.current) {
-  //     formRef.current.reset()
-  //   }
-  // }
+  const handleReset = () => {
+    if (formRef.current) {
+      formRef.current.reset()
+    }
+  }
   const categories = Object.keys(CategoriesEnum).filter((key) =>
     Number.isNaN(Number(key))
   )
 
   return (
     // 🐶 Ajoute le prop `action={formAction}`
-    <form ref={formRef} className="gap-2 space-y-4">
+    <form ref={formRef} action={formAction} className="gap-2 space-y-4">
       <Label>Product title</Label>
       <Input placeholder="ex : Iphone" name="title" />
       <Label>Product title</Label>
@@ -67,13 +76,22 @@ export default function ProductForm({product}: {product?: Product}) {
       <Label>Product title</Label>
       <Input type="number" placeholder="Product quantity" name="quantity" />
       <div className="flex gap-2">
-        <Button size="sm" type="submit">
-          Save
-        </Button>
-        <Button size="sm" variant="outline">
-          Cancel
-        </Button>
+        <Buttons />
       </div>
     </form>
+  )
+}
+
+function Buttons() {
+  const {pending} = useFormStatus()
+  return (
+    <>
+      <Button size="sm" type="submit" disabled={pending}>
+        Save
+      </Button>
+      <Button size="sm" variant="outline">
+        Cancel
+      </Button>
+    </>
   )
 }
